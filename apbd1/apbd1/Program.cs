df4aee4b171e0c6fa91417d79d25f4e513750162
Mateusz Utrata <s31252@pjwstk.edu.pl>
2025-03-04 15:08:03 +0100

    Modyfikacja 4

--- apbd1/apbd1/Program.cs
+++ apbd1/apbd1/Program.cs
@@ -2,3 +2,11 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Nie no to był żart sorki");
 Console.WriteLine("mod3");
+
+ static double GetAverage(int[] arr)
+{
+    return 0;
+}
+
+int[] arr = { 3, 5, 7, 8 };
+GetAverage(arr);
